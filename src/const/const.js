5dd468734be07f8e27a599d7a66b0ff9984464a0
Luis Mayta <luis@hadenlabs.com>
2024-09-29 09:00:08 -0300

    chore(core): implement information for siru (#2)

---
+++ src/const/const.js
@@ -0,0 +1,363 @@
+export const fights = [
+  {
+    opponent: "Carlos Roberto Carrizo",
+    date: "12 de abril de 2002",
+    result: "Empate por decisión (PTS)"
+  },
+  {
+    opponent: "Alejandro Demetrio González",
+    date: "29 de noviembre de 2002",
+    result: "Victoria por KO (3)"
+  },
+  {
+    opponent: "Fernando Javier Gutiérrez",
+    date: "21 de marzo de 2003",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Carlos Andrés Monzón",
+    date: "14 de noviembre de 2003",
+    result: "Victoria por KO (5)"
+  },
+  {
+    opponent: "David Fernando Morán",
+    date: "10 de julio de 2004",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Sergio Alberto Martínez",
+    date: "16 de abril de 2005",
+    result: "Derrota por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Marcos René Maidana",
+    date: "12 de noviembre de 2005",
+    result: "Derrota por KO (6)"
+  },
+  {
+    opponent: "Rubén Darío Rojas",
+    date: "3 de marzo de 2007",
+    result: "Victoria por KO (4)"
+  },
+  {
+    opponent: "Ariel Eduardo Becerra",
+    date: "10 de noviembre de 2007",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Rogelio Omar Rojas",
+    date: "21 de marzo de 2008",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Jorge Luis Rodríguez",
+    date: "18 de julio de 2009",
+    result: "Victoria por KO (2)"
+  },
+  {
+    opponent: "Hugo Pineda",
+    date: "29 de noviembre de 2009",
+    result: "Derrota por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Adrián Elías Fernández",
+    date: "25 de febrero de 2010",
+    result: "Victoria por KO (3)"
+  },
+  {
+    opponent: "Matías Ariel López",
+    date: "14 de agosto de 2010",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Marcelo Ariel Rodríguez",
+    date: "30 de enero de 2011",
+    result: "Victoria por KO (2)"
+  },
+  {
+    opponent: "Ezequiel Marcelo López",
+    date: "29 de octubre de 2011",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Diego Armando Ramos",
+    date: "25 de febrero de 2012",
+    result: "Victoria por KO (4)"
+  },
+  {
+    opponent: "Juan Carlos Abarca",
+    date: "1 de diciembre de 2012",
+    result: "Victoria por KO (5)"
+  },
+  {
+    opponent: "Walter Adrián Sequeira",
+    date: "5 de diciembre de 2013",
+    result: "Victoria por KO (2)"
+  },
+  {
+    opponent: "Julián Andrés Ospina",
+    date: "12 de abril de 2014",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Miguel Angel Cotto",
+    date: "12 de octubre de 2014",
+    result: "Derrota por KO (6)"
+  },
+  {
+    opponent: "Elías José Cabrera",
+    date: "10 de agosto de 2015",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "Emilio Enrique Nascimento",
+    date: "3 de octubre de 2015",
+    result: "Victoria por KO (4)"
+  },
+  {
+    opponent: 'Fabián "Cobra" Duran',
+    date: "25 de marzo de 2016",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: "José Fernando Castillo",
+    date: "11 de junio de 2016",
+    result: "Victoria por KO (5)"
+  },
+  {
+    opponent: "Eduardo Javier Orozco",
+    date: "15 de octubre de 2016",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Nicolás "Pibe" Galarza',
+    date: "12 de noviembre de 2016",
+    result: "Victoria por KO (3)"
+  },
+  {
+    opponent: "Daniel Eduardo Pacheco",
+    date: "15 de julio de 2017",
+    result: "Victoria por KO (7)"
+  },
+  {
+    opponent: 'Patricio "Pato" Pérez',
+    date: "30 de septiembre de 2017",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Rafael "La Hiena" Barrios',
+    date: "10 de diciembre de 2017",
+    result: "Derrota por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Fabián "Boca" Martínez',
+    date: "12 de enero de 2018",
+    result: "Victoria por KO (3)"
+  },
+  {
+    opponent: 'Joaquín "La Bestia" Moya',
+    date: "14 de abril de 2018",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Sergio "El Zorro" Pineda',
+    date: "28 de julio de 2018",
+    result: "Victoria por KO (5)"
+  },
+  {
+    opponent: 'Ezequiel "El Chino" Silva',
+    date: "27 de octubre de 2018",
+    result: "Victoria por KO (6)"
+  },
+  {
+    opponent: 'David "El Mago" González',
+    date: "19 de enero de 2019",
+    result: "Victoria por KO (4)"
+  },
+  {
+    opponent: 'Leonardo "El Chino" Hernández',
+    date: "16 de marzo de 2019",
+    result: "Derrota por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Joaquín "El Pato" Pérez',
+    date: "12 de octubre de 2019",
+    result: "Victoria por KO (3)"
+  },
+  {
+    opponent: 'Emiliano "La Cobra" Ledesma',
+    date: "7 de diciembre de 2019",
+    result: "Victoria por decisión unánime (PTS)"
+  },
+  {
+    opponent: 'Diego "El Chino" Silva',
+    date: "16 de enero de 2021",
+    result: "Victoria por KO (2)"
+  }
+]
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite"
+  // here add more icon keys
+}
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi
+  },
+
+  {
+    title: "Redux Js",
+    icon: IconKeys.redux
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript
+  },
+  {
+    title: "Radix UI",
+    icon: IconKeys.radix
+  },
+
+  {
+    title: "React Query",
+    icon: IconKeys.reactQuery
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite
+  },
+  {
+    title: "Zustand",
+    icon: IconKeys.zustand
+  },
+
+  {
+    title: "Shadcn UI",
+    icon: IconKeys.shadcn
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb
+  }
+]
+
+export const boxingTitles = [
+  {
+    img: "../../../img/south-american-champion.jpg",
+    title: "Campeón Sudamericano de Peso Supermediano",
+    link: "https://boxrec.com/en/proboxer/157842",
+    description: `Rubén "Siru" Eduardo Acosta ganó el título de Campeón Sudamericano de Peso Supermediano. Este fue uno de sus logros más destacados en el boxeo regional.`,
+    technologies: [
+      {
+        icon: IconKeys.boxing
+      },
+      {
+        icon: IconKeys.training
+      },
+      {
+        icon: IconKeys.titles
+      }
+    ]
+  },
+  {
+    img: "../../../img/argentine-champion.jpg",
+    title: "Campeón Argentino de Peso Supermediano",
+    link: "https://boxrec.com/en/proboxer/157842",
+    description: `Rubén "Siru" Eduardo Acosta se consagró Campeón Argentino de Peso Supermediano, consolidando su dominio en el circuito nacional.`,
+    technologies: [
+      {
+        icon: IconKeys.boxing
+      },
+      {
+        icon: IconKeys.training
+      },
+      {
+        icon: IconKeys.titles
+      }
+    ]
+  },
+  {
+    img: "../../../img/nabo-cruiserweight.jpg",
+    title: "Campeón NABO de Peso Crucero de la WBO",
+    link: "https://boxrec.com/en/proboxer/157842",
+    description: `Acosta alcanzó el título internacional de la NABO en la categoría Peso Crucero, bajo la Organización Mundial de Boxeo (WBO).`,
+    technologies: [
+      {
+        icon: IconKeys.boxing
+      },
+      {
+        icon: IconKeys.training
+      },
+      {
+        icon: IconKeys.titles
+      }
+    ]
+  }
+]
